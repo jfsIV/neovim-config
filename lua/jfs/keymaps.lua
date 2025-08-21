a676491f14d17260beb802a496225cf44b3345bc
Joseph F. Specht IV <jfspecht@ncsu.edu>
2025-08-21 14:37:56 -0400

    adding a few more packages

--- lua/jfs/keymaps.lua
+++ lua/jfs/keymaps.lua
@@ -1,9 +1,30 @@
-vim.g.mapleader = " "  -- space is the leader character for custom keymaps
+-- space is the leader character for custom keymaps
+vim.g.mapleader = " "
 
-vim.keymap.set("n", "<Esc>", "<Esc>:noh<CR><Esc>")  -- Esc unhighlights matches
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)  -- opens the directory view in vim
+-- Esc unhighlights matches
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+-- opens the directory view in vim
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")  -- yanks highlighted text to SYSTEM keyboard
---vim.keymap.set({"n", "v"}, "<leader>Y", "\"+Y")  -- yanks current line to SYSTEM keyboard
+-- yanks highlighted text to SYSTEM keyboard
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
+-- yanks current line to SYSTEM keyboard
+--vim.keymap.set({"n", "v"}, "<leader>Y", "\"+Y")
 
-vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")  -- deletes highlighted text to the void
+-- deletes highlighted text to the void
+vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+
+-- exit terminal mode. might not work if you are using a terminal emulator :/
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
+
+-- grow up!!!
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- move to other windows quicker
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- lua/jfs/settings.lua
+++ lua/jfs/settings.lua
@@ -1,31 +1,73 @@
-vim.opt.guicursor = ""  -- cursor to solid in all modes, really sets it to default
+-- cursor to solid in all modes
+vim.opt.guicursor = ""
 
-vim.opt.nu = true  -- line numbers
-vim.opt.relativenumber = true  -- relative line numbers for quick jk
+-- line numbers
+vim.opt.nu = true
+-- relative line numbers for quick jk
+vim.opt.relativenumber = true
+-- highlight cursor line
+vim.opt.cursorline = true
 
-vim.opt.tabstop = 4  -- tabs are 4 spaces
-vim.opt.shiftwidth = 4  -- 4 whitespaces used for one level of indentation
-vim.opt.softtabstop = 4  -- spaces added or removed by tab or backspace
-vim.opt.expandtab = true  -- tabs get turned into spaces
+-- tabs are 4 spaces
+vim.opt.tabstop = 4
+-- 4 whitespaces used for one level of indentation
+vim.opt.shiftwidth = 4
+-- spaces added or removed by tab or backspace
+vim.opt.softtabstop = 4
+-- tabs get turned into spaces
+vim.opt.expandtab = true
 
-vim.opt.smartindent = false -- messes up default indents
+-- messes up default indents
+vim.opt.smartindent = false
 
-vim.opt.wrap = true  -- wrap lines longer than screensize
+-- wrap lines longer than screensize
+vim.opt.wrap = true
 
-vim.opt.swapfile = false  -- does not save swap files
-vim.opt.backup = false  -- does not save backups
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- dir for undo files
-vim.opt.undofile = true  -- saves undo files
+-- does not save swap files
+vim.opt.swapfile = false
+-- does not save backups
+vim.opt.backup = false
+-- dir for undo files
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- saves undo files
+vim.opt.undofile = true
 
-vim.opt.hlsearch = true  -- highlight all matching searches
-vim.opt.incsearch = true  -- highlights as you enter in search
+-- highlight all matching searches
+vim.opt.hlsearch = true
+-- highlights as you enter in search
+vim.opt.incsearch = true
+-- incremental substitutions
+vim.opt.inccommand = "split"
 
-vim.opt.termguicolors = true  -- use the color scheme of the terminal
+-- use the color scheme of the terminal
+vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8  -- always have at least 8 spaces above or below code
-vim.signcolum = "yes"  -- all column tags are shown
-vim.opt.isfname:append("@-@")  -- appends the current file to the path?
+-- always have at least 8 spaces above or below code
+vim.opt.scrolloff = 8
+-- all column tags are shown
+vim.signcolum = "yes"
+-- appends the current file to the path?
+vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 750  -- time[ms] after typing before showing suggestions
+-- time[ms] after typing before showing suggestions
+vim.opt.updatetime = 750
+-- time before the previous keystroke is ignored
+vim.opt.timeoutlen = 800
 
-vim.opt.colorcolumn = "81"  -- shows when you are past column 81
+-- shows when you are past column 81
+vim.opt.colorcolumn = "81"
+
+-- if you search in all lowercase, it is case insensitive
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- lines that get wrapped to next line keep the same indentation
+vim.opt.breakindent = true
+
+-- sets how to display certain whitespace characters
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
